d2980c066780cde0d566bc9f0f4155db9947d4b2
William Louth <williamlouth@outlook.com>
2024-04-11 18:39:38 +0100

    Trying to get nvimtree working... failing

--- init.lua
+++ init.lua
@@ -538,7 +538,15 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          cmd = { 'clangd', '--background-index' },
+          settings = {
+            ['clangd'] = {
+              ['fallbackFlags'] = { '-std=c++23' },
+              ['Compiler'] = { 'g++' },
+            },
+          },
+        },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -847,7 +855,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
