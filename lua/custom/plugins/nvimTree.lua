6aa23f5b397fe8d089785545ca7fe38dcb7a5f16
William Louth <williamlouth@outlook.com>
2024-04-14 14:03:52 +0100

    NvimTree is a pain

--- lua/custom/plugins/nvimTree.lua
+++ lua/custom/plugins/nvimTree.lua
@@ -1,10 +1,30 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 local function my_on_attach(bufnr)
   local api = require 'nvim-tree.api'
 
   local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, noawait = true }
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
   end
+
+  local function collapse()
+    local api = require 'nvim-tree.api'
+    api.node.navigate.parent()
+    api.node.open.edit()
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', 'l', api.node.open.edit, opts 'open')
+  vim.keymap.set('n', 'h', collapse, opts 'close')
 end
+
+local function open_nvim_tree()
+  require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+
 return {
   'nvim-tree/nvim-tree.lua',
   config = function()
