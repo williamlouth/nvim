2b82deae6a61d9f07047e8c07b1ef96a9449c298
William Louth <williamlouth@outlook.com>
2024-04-14 14:35:28 +0100

    Switch to behaviour i like for selecting suggestions

--- init.lua
+++ init.lua
@@ -710,7 +710,8 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<tab>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = true},
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
