dc02983db241b6d79142974b494574d4f1f6bb97
william louth <williamlouth@outlook.com>
2024-04-25 11:56:50 +0100

    open neotest summary tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,14 +5,23 @@ vim.keymap.set('n', '<F6>', require('dap').toggle_breakpoint, { desc = 'Add Brea
 vim.keymap.set('n', '<F7>', require('dap').step_into, { desc = 'Step Into' })
 vim.keymap.set('n', '<F8>', require('dap').step_over, { desc = 'Step Over' })
 vim.keymap.set('n', '<F9>', require('dap').continue, { desc = 'Continue' })
-vim.keymap.set('n', '<s-F10>', require('neotest').run.run, { desc = 'run nearest test' })
-vim.keymap.set('n', '<F22>', require('neotest').run.run, { desc = 'run nearest test' })
-vim.keymap.set('n', '<F24>', function()
+
+local function runNearest()
+  require('neotest').summary.close()
+  require('neotest').summary.open()
+  require('neotest').run.run()
+end
+local function runAll()
+  require('neotest').summary.close()
+  require('neotest').summary.open()
   require('neotest').run.run(vim.fn.expand '%')
-end, { desc = 'summary open' })
-vim.keymap.set('n', '<s-F12>', function()
-  require('neotest').run.run(vim.fn.expand '%')
-end, { desc = 'summary open' })
+end
+
+vim.keymap.set('n', '<s-F10>', runNearest, { desc = 'run nearest test' })
+vim.keymap.set('n', '<F22>', runNearest, { desc = 'run nearest test' })
+
+vim.keymap.set('n', '<F24>', runAll, { desc = 'run all tests' })
+vim.keymap.set('n', '<s-F12>', runAll, { desc = 'run all tests' })
 
 vim.keymap.set('n', '<leader>Q', require('dapui').toggle, { desc = 'Open dapui' })
 vim.keymap.set('n', '<leader>W', require('dap').repl.open, { desc = 'Open repl' })
