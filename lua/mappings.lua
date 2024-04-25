d2a4837c99d5479a0481b37794828db29fdc5209
william louth <williamlouth@outlook.com>
2024-04-25 11:36:05 +0100

    Adding shift f keys for mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,8 +5,12 @@ vim.keymap.set('n', '<F6>', require('dap').toggle_breakpoint, { desc = 'Add Brea
 vim.keymap.set('n', '<F7>', require('dap').step_into, { desc = 'Step Into' })
 vim.keymap.set('n', '<F8>', require('dap').step_over, { desc = 'Step Over' })
 vim.keymap.set('n', '<F9>', require('dap').continue, { desc = 'Continue' })
+vim.keymap.set('n', '<s-F10>', require('neotest').run.run, { desc = 'run nearest test' })
 vim.keymap.set('n', '<F22>', require('neotest').run.run, { desc = 'run nearest test' })
 vim.keymap.set('n', '<F24>', function()
+  require('neotest').run.run(vim.fn.expand '%')
+end, { desc = 'summary open' })
+vim.keymap.set('n', '<s-F12>', function()
   require('neotest').run.run(vim.fn.expand '%')
 end, { desc = 'summary open' })
 
